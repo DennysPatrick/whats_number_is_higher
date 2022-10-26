f14113cb38e00d92cdae5e499b53fd82f791e23c
Dennys Patrick <dennyspatrickoliveira@hotmail.com>
2022-10-26 19:18:49 -0300

    first commit

---
+++ projeto01/projeto01.py
@@ -0,0 +1,9 @@
+num_01 = int(input('Digite um número:'))
+num_02 = int(input('Digite outro número: '))
+
+if num_01 > num_02:
+    print('O número ' + str(num_01) + ' é maior!')
+elif num_01 < num_02:
+    print('O número ' + str(num_02) + ' é maior!')
+else:
+    print('Os dois numeros são iguais!')
